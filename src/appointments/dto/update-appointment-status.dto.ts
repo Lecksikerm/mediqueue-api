3d90a0d0bb31fd2d980fc08b8da65ed85100c9eb
kareem idris <idrisolalekann@gmail.com>
2026-06-12 15:37:49 +0100

    Update swagger to appointment dto

--- src/appointments/dto/update-appointment-status.dto.ts
+++ src/appointments/dto/update-appointment-status.dto.ts
@@ -1,7 +1,13 @@
 import { IsEnum, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { AppointmentStatus } from '../../common/enums';
 
 export class UpdateAppointmentStatusDto {
+  @ApiProperty({
+    enum: AppointmentStatus,
+    example: AppointmentStatus.IN_PROGRESS,
+    description: 'New status for the appointment',
+  })
   @IsNotEmpty()
   @IsEnum(AppointmentStatus)
   status: AppointmentStatus;
